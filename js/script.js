18a4acee5ceaa2a0a203067a0eaec0845dc8ca1b
Dujardin fabrice <fabricedujardin873@gmail.com>
2020-10-30 09:10:42 +1100

    rajout de modification de scrool en jquery et du css

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ window.transitionToPage = function(href) {
        document.querySelector('body').style.opacity = 0
        setTimeout(function() { 
            window.location.href = href
-       }, 500)
+       }, 700)
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-       document.querySelector('body').style.opacity = 1
+    document.querySelector('body').style.opacity = 1;
 })
